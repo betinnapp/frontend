725090b7203540fde7548a887c0390213e323a43
Rafael Cineis <rafael.cineis@hotmail.com>
2019-08-22 20:11:15 -0300

    Setting default request timeout

--- app/utils/request.js
+++ app/utils/request.js
@@ -1,8 +1,11 @@
 import axios from 'axios'
 
+const REQUEST_TIMEOUT = 30000
+
 export default function request(url, options) {
   const newOptions = {
     url,
+    timeout: REQUEST_TIMEOUT,
     ...options,
   }
 
