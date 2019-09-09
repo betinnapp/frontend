c4340e9d7e7c414c5fb577bccf83fabe1da43187
Rafael Cineis <rafael.cineis@hotmail.com>
2019-09-08 21:27:44 -0300

    Styling better the Welcome page

--- app/containers/WelcomePage/index.js
+++ app/containers/WelcomePage/index.js
@@ -6,30 +6,63 @@
 
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 import { connect } from 'react-redux'
 import { FormattedMessage } from 'react-intl'
 import { createStructuredSelector } from 'reselect'
 import { compose } from 'redux'
 
 import Button from 'components/Button'
+import ContentWrapper from 'components/ContentWrapper'
+import Icon from 'images/icon.svg'
+import Text from 'components/Text'
 
 import messages from './messages'
 
+const Wrapper = styled(ContentWrapper)`
+  .header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 8px 32px;
+    color: #767676;
+    background-color: #fff;
+
+    img {
+      width: 45px;
+    }
+  }
+
+  .footer {
+    text-align: center;
+    margin-bottom: 16px;
+  }
+`
+
 export function WelcomePage() {
   return (
-    <div>
-      <div>
-        <FormattedMessage
-          {...messages.welcomeUser}
-          values={{ username: 'Test' }}
-        />
+    <Wrapper
+      grid
+      fullHeight
+      noLateralMargins
+      gridTemplateRows={['80px', '1fr', 'auto']}
+    >
+      <div className="header">
+        <Text bold medium>
+          <FormattedMessage
+            {...messages.welcomeUser}
+            values={{ username: 'Test' }} // TODO: Use username from stored user
+          />
+        </Text>
+        <img src={Icon} alt="User" />
       </div>
-      <div>
+      <div>{/* TODO: Quick modules list */}</div>
+      <div className="footer">
         <Button id="seeAvailableModules" link="/modules" small>
           <FormattedMessage {...messages.seeAvailableModules} />
         </Button>
       </div>
-    </div>
+    </Wrapper>
   )
 }
 
