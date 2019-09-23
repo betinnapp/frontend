deb53c1efcf1476b33258bb42c860b0a63b4a931
Rafael Cineis <rafael.cineis@hotmail.com>
2019-09-23 19:27:05 -0300

    Creating mock and api responses for getting and posting quiz

--- app/containers/App/urls.js
+++ app/containers/App/urls.js
@@ -2,6 +2,7 @@ const BASE_API_PATH = ''
 const AUTH_API_PATH = `${BASE_API_PATH}/auth`
 const USER_API_PATH = `${BASE_API_PATH}/user`
 const EDU_API_PATH = `${BASE_API_PATH}/module`
+const SURVEY_API_PATH = `${BASE_API_PATH}/survey`
 
 export const LOGIN_API_URL = `${AUTH_API_PATH}/login`
 
@@ -11,6 +12,9 @@ export const USER_DETAILS_API_URL = `${USER_API_PATH}/me`
 export const MODULES_LIST_API_URL = `${EDU_API_PATH}/list`
 export const MODULE_DETAILS_API_URL = `${EDU_API_PATH}/:moduleId`
 export const SUBMODULE_API_URL = `${MODULE_DETAILS_API_URL}/submodule/:submoduleId`
+
+export const SURVEY_API_URL = `${SURVEY_API_PATH}/:surveyId`
+export const SURVEY_ANSWER_API_URL = `${SURVEY_API_URL}/answers`
 
 // Routes Path
 export const LOGIN_PATH = '/login'
