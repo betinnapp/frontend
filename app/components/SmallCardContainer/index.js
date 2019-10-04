ca8523f23fef87d638691d87509be7db0c172ee7
Rafael Cineis <rafael.cineis@hotmail.com>
2019-10-03 22:28:43 -0300

    Creating SmallCardContainer component

---
+++ app/components/SmallCardContainer/index.js
@@ -0,0 +1,23 @@
+/**
+ *
+ * SmallCardContainer
+ *
+ */
+
+import styled from 'styled-components'
+
+const SmallCardContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 0 16px;
+  align-items: center;
+
+  min-height: 56px;
+  background: #fff;
+  margin-bottom: 16px;
+  border-radius: 8px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+`
+
+export default SmallCardContainer
