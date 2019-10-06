296e289d824749a6b24f9964d9f72bdcaf2c68d4
Rafael Cineis <rafael.cineis@hotmail.com>
2019-10-06 16:37:45 -0300

    Dividing the interestRate value by 100 to match the expected value

--- app/containers/GoalDetailsPage/InvestimentPreview.js
+++ app/containers/GoalDetailsPage/InvestimentPreview.js
@@ -22,7 +22,7 @@ export function InvestimentPreview(props) {
   let total = depositTotal
   let totalIncome = 0
   let totalTax = 0
-  const monthTaxRate = (investimentType.interestRate || 0) / 12
+  const monthTaxRate = ((investimentType.interestRate || 0) / 100) / 12
 
   for (let i = 0; i < values.duration; i += 1) {
     const monthlyIncome = total * monthTaxRate
